1d74d8f2e3a01a7918d543322018a561ce1e056b
ACuriel2 <antonio.curiel@tralix.com>
2015-04-14 10:35:33 -0500

    Refactor - Gutter Game Test - Create Game test deleted, Game variable is private and initialized at Before statement

--- BowlingGame/src/com/curiel/kata/bowlinggame/testing/KataTesting.java
+++ BowlingGame/src/com/curiel/kata/bowlinggame/testing/KataTesting.java
@@ -2,20 +2,21 @@ package com.curiel.kata.bowlinggame.testing;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.curiel.kata.bowlinggame.project.Game;
 
 public class KataTesting {
-
-	@Test
-	public void createGame() {
-		Game game = new Game();
+	private Game game;
+	
+	@Before
+	public void setup(){
+		game = new Game();		
 	}
 	
 	@Test
 	public void testGutterGame(){
-		Game game = new Game();
 		for(int i = 1; i<=20; i++)
 			game.roll(0);
 		assertEquals(0, game.score());
