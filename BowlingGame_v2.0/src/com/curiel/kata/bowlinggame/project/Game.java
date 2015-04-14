2ffc1193eb295572b513cad80d4ebece67b23be5
ACuriel2 <antonio.curiel@tralix.com>
2015-04-14 17:56:02 -0500

    Red

---
+++ BowlingGame_v2.0/src/com/curiel/kata/bowlinggame/project/Game.java
@@ -0,0 +1,12 @@
+package com.curiel.kata.bowlinggame.project;
+
+public class Game {
+
+	public void roll(int fallenPines) {		
+	}
+
+	public int score() {		
+		return -1;
+	}
+
+}
--- BowlingGame_v2.0/src/com/curiel/kata/bowlinggame/testing/KataTesting.java
+++ BowlingGame_v2.0/src/com/curiel/kata/bowlinggame/testing/KataTesting.java
@@ -4,10 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.curiel.kata.bowlinggame.project.Game;
+
 public class KataTesting {
 
 	@Test
-	public void test() {
+	public void testGutterGame() {
+		Game game = new Game();
+		for(int i=0; i<20; i++)
+			game.roll(0);
+		assertEquals(0, game.score());
 	}
 
 }
