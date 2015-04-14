4ea897ea3b962da1e06f59d127b997ada379c7cd
ACuriel2 <antonio.curiel@tralix.com>
2015-04-14 12:34:44 -0500

    Red - Gutter Game Test The score method returns -1

--- BowlingGame_v2.0/src/com/curiel/kata/bowlinggame/testing/KataTesting.java
+++ BowlingGame_v2.0/src/com/curiel/kata/bowlinggame/testing/KataTesting.java
@@ -4,11 +4,15 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.curiel.kata.bowlinggame.project.Game;
+
 public class KataTesting {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGutterGame() {
+		Game game = new Game();
+		game.roll(0);
+		assertEquals(0, game.score());
 	}
 
 }
