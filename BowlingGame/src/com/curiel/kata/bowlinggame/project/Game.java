796bd90e3fdf2fae61bbab8088a2f09a4aa16db9
ACuriel2 <antonio.curiel@tralix.com>
2015-04-14 10:27:00 -0500

    Red - Gutter Game Test

--- BowlingGame/src/com/curiel/kata/bowlinggame/project/Game.java
+++ BowlingGame/src/com/curiel/kata/bowlinggame/project/Game.java
@@ -2,4 +2,11 @@ package com.curiel.kata.bowlinggame.project;
 
 public class Game {
 
+	public void roll(int fallenPines) {		
+	}
+
+	public int score() {
+		return -1;
+	}
+
 }
--- BowlingGame/src/com/curiel/kata/bowlinggame/testing/KataTesting.java
+++ BowlingGame/src/com/curiel/kata/bowlinggame/testing/KataTesting.java
@@ -12,5 +12,13 @@ public class KataTesting {
 	public void createGame() {
 		Game game = new Game();
 	}
+	
+	@Test
+	public void testGutterGame(){
+		Game game = new Game();
+		for(int i = 1; i<=20; i++)
+			game.roll(0);
+		assertEquals(0, game.score());
+	}
 
 }
