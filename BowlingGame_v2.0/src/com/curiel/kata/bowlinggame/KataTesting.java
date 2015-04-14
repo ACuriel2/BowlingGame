3416e17c2af52a1b624090512fd5b9a2872aa1f9
ACuriel2 <antonio.curiel@tralix.com>
2015-04-14 12:26:55 -0500

    Project Creation. V2 of Bowling Game Kata

---
+++ BowlingGame_v2.0/src/com/curiel/kata/bowlinggame/KataTesting.java
@@ -0,0 +1,14 @@
+package com.curiel.kata.bowlinggame;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KataTesting {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
