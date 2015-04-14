733eff8b7a2286c9becac69a64e9c0ea5569f276
ACuriel2 <antonio.curiel@tralix.com>
2015-04-14 12:38:26 -0500

    Red - All Ones Game Test

--- BowlingGame_v2.0/src/com/curiel/kata/bowlinggame/testing/KataTesting.java
+++ BowlingGame_v2.0/src/com/curiel/kata/bowlinggame/testing/KataTesting.java
@@ -11,8 +11,17 @@ public class KataTesting {
 	@Test
 	public void testGutterGame() {
 		Game game = new Game();
-		game.roll(0);
+		for(int i = 1; i<= 20; i++)
+			game.roll(0);
 		assertEquals(0, game.score());
+	}
+	
+	@Test
+	public void testAllOnesGame(){
+		Game game = new Game();
+		for(int i = 1; i<= 20; i++)
+			game.roll(1);
+		assertEquals(20, game.score());
 	}
 
 }
