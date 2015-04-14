9fc57cb3fab32ab0c263fd4549114e71f4ee8e7b
ACuriel2 <antonio.curiel@tralix.com>
2015-04-14 12:39:44 -0500

    Green - All Ones Game Test

--- BowlingGame_v2.0/src/com/curiel/kata/bowlinggame/project/Game.java
+++ BowlingGame_v2.0/src/com/curiel/kata/bowlinggame/project/Game.java
@@ -1,12 +1,18 @@
 package com.curiel.kata.bowlinggame.project;
 
 public class Game {
+	private int score;
+	
+	public Game() {
+		score = 0;
+	}
 
-	public void roll(int fallenPines) {		
+	public void roll(int fallenPines) {
+		score += fallenPines;
 	}
 
 	public int score() {
-		return 0;
+		return score;
 	}
 
 }
