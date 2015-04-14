e7f3721448a7cd5a3f3c1008c3aa187c1795a1e9
ACuriel2 <antonio.curiel@tralix.com>
2015-04-14 17:58:54 -0500

    Refactor

--- BowlingGame_v2.0/src/com/curiel/kata/bowlinggame/testing/KataTesting.java
+++ BowlingGame_v2.0/src/com/curiel/kata/bowlinggame/testing/KataTesting.java
@@ -2,18 +2,28 @@ package com.curiel.kata.bowlinggame.testing;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.curiel.kata.bowlinggame.project.Game;
 
 public class KataTesting {
+	Game game;
+	
+	@Before
+	public void setup(){
+		game = new Game();
+	}
 
 	@Test
 	public void testGutterGame() {
-		Game game = new Game();
-		for(int i=0; i<20; i++)
-			game.roll(0);
+		rollMany(0, 20);
 		assertEquals(0, game.score());
+	}
+
+	private void rollMany(int pines, int rolls) {
+		for(int i=1; i<=rolls; i++)
+			game.roll(pines);
 	}
 
 }
