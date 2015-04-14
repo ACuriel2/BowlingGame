af96571b7b421f3f93a0e9356bbf714767c47458
ACuriel2 <antonio.curiel@tralix.com>
2015-04-14 18:03:16 -0500

    Refactor

--- BowlingGame_v2.0/src/com/curiel/kata/bowlinggame/project/Game.java
+++ BowlingGame_v2.0/src/com/curiel/kata/bowlinggame/project/Game.java
@@ -1,17 +1,22 @@
 package com.curiel.kata.bowlinggame.project;
 
 public class Game {
-	private int score;
+	private int rollCounter;
+	private int[] rolls;
 	
-	public Game() {
-		score = 0;
+	public Game() {		
+		rollCounter = 0;
+		rolls = new int[21];
 	}
 
 	public void roll(int fallenPines) {
-		score += fallenPines;
+		rolls[rollCounter++] = fallenPines;
 	}
 
-	public int score() {		
+	public int score() {
+		int score = 0;
+		for(int pinesByRoll : rolls)
+			score+=pinesByRoll;
 		return score;
 	}
 
